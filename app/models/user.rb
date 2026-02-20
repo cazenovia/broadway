c3e9f90d05a131780f89bb88c54fa58ade6f574d
cazenovia <tdurkin@cazenoviasoftware.com>
2026-02-20 19:02:22 +0000

    added missing relation on User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+  has_many :sessions, dependent: :destroy
   has_many :tickets
   has_many :ticket_notes
   has_many :contacts, dependent: :destroy
