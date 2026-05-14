72342deefe6ec641682b6b877def4e0821019950
Tim Durkin <tdurkin@cazenoviasoftware.com>
2026-05-14 17:41:05 -0400

    fixed seed for user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # Ensure we have our default Field Worker
 # find_or_create_by ensures this is safe to run multiple times!
-User.find_or_create_by!(email: "worker@broadway.app") do |user|
-  user.name = "Field Worker 1"
+User.find_or_create_by!(email_address: "baltimoretim@gmail.com") do |user|
+  user.name = "Tim Durkin"
 end
