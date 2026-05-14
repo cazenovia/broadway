1e871eaad2ff9d8cd3285a8f763a1e3d524bb901
Tim Durkin <tdurkin@cazenoviasoftware.com>
2026-05-14 17:46:18 -0400

    added default password for user

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,5 @@
 # find_or_create_by ensures this is safe to run multiple times!
 User.find_or_create_by!(email_address: "baltimoretim@gmail.com") do |user|
   user.name = "Tim Durkin"
+  user.password = "Magik26!"
 end
